137c4b30faf8bb1c97becafc5b212bbeb0eb29c7
麻瓜瓜 <96295450+MagicstMagoo@users.noreply.github.com>
2022-04-19 13:33:50 +0800

    新包名

---
+++ src/main/cn/sculk/magicst/LangChancePlus/rules.java
@@ -0,0 +1,14 @@
+package cn.sculk.magicst;
+
+import net.fabricmc.api.ModInitializer;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public rule1 ("rsplus"){
+
+
+@Override treakerroo
+
+}
+
+@Nullable void ([])
